5f5eb29034099ce3791709af3c0c503ea3555b43
Nick Stone <stone.nicholas@gmail.com>
2017-03-15 22:52:48 -0400

    Added a forceNonFlatFits function which, if one round of fixFlatFits() doesn't result in all fits being fixed slowly increases CPMaxF for remaining fits until no flat fits are left

--- Rasylum.R
+++ Rasylum.R
@@ -1,4 +1,4 @@
-                                        #Version 0.2.0
+                                        #Version 0.2.1
 
 buildFrame=function(dataList, numRows){
                                         #Transforms dataList, which is a list of lists with the same member names into a data.table with those names as columns
@@ -259,6 +259,7 @@ stiffnessSphereOnPlane=function(rBead, extZ, extForce, CPMaxF=.05, percentToFit=
                                         #remove long approach curve
         trimmedStart=floor(roughCPIndex-approachTrim*length(extForce))
     }
+    roughCPIndex=roughCPIndex-(trimmedStart-1)
     extZ=extZ[trimmedStart:length(extZ)]
     extForce=extForce[trimmedStart:length(extForce)]
     
@@ -495,6 +496,35 @@ fixFlatFits=function(fits, minRise=.1,debug=FALSE, numCores=-1){
         }
     }
     return(fits)
+}
+
+forceNonFlatFits=function(fits,minRise=0.3,numCores=-1){
+    notDone=fits
+    fixedFits=fits
+    fixedFits$fits=list()
+    for(rise in seq(from=fits$CPMaxF,to=1,by=fits$CPMaxF)){
+        print(paste("CPMaxF=",rise,sep=""))
+        notDone$CPMaxF=rise
+        newFits=fixFlatFits(notDone,minRise,FALSE,numCores)
+        notDone=newFits
+        doneIndices=c()
+        for(i in 1:length(notDone$fits)){
+            fit=notDone$fits[[i]]
+            curves=fit$fit$curves
+            measured=curves[curves$curve=="measured",]$F
+            model=curves[curves$curve=="model",]$F
+            if((max(model)/max(measured))>=minRise){
+                                        #Done!
+                fixedFits$fits[[length(fixedFits$fits)+1]]=notDone$fits[[i]]
+                doneIndices=c(doneIndices,i)
+            }
+        }
+        notDone$fits=notDone$fits[-1*doneIndices]
+        if(length(notDone$fits)<1){
+            break
+        }
+    }
+    return(fixedFits)
 }
 
 extractTimeConst=function(frame, time="t", force="force", zPos="zSensr", dwellTime="dwell", debug=FALSE){
