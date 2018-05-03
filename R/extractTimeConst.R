3a4f8f59fbd51e5cc4e4dd544c676b147e7fe92d
Nick Stone <stone.nicholas@gmail.com>
2018-05-03 13:56:38 -0400

    Added code to estimate better initial guesses for nls in extractTimeConst

--- R/extractTimeConst.R
+++ R/extractTimeConst.R
@@ -2,10 +2,17 @@ extractTimeConst=function(frame, time="t", force="force", zPos="zSensr", dwellTi
                                         #frame is the raw data for the compression as produced by the loadIBW function. time, force, zPos and dwellTime are the names of the columns of frame which contain the time, force, indentation, and dwellTime data. Defaults correspond to the values used in data imported using loadIBW, batchLoad and quickLoad. Setting debug to TRUE causes the function to plot each fit (the user should press 'enter' to cycle through fits) in order to allow for tuning of fit parameters.
     ret=getDwell(frame,frame[1,dwellTime],zPos,force,time,extraTrim)
     decayData=data.frame(t=ret[,time]-ret[1,time],F=ret[,force],FZero=ret[1,force],zPos=ret[,zPos])
+    cGuess=decayData$F[length(decayData$F)]
+    aGuess=decayData$F[1]-cGuess
+    eThreshold=decayData$FZero[1]-.63*aGuess #Point where force has decayed by 1/e
+    eTime=decayData[decayData$F<eThreshold,]$t[1] #time where force has decayed by 1/e
+    tau1guess=1/eTime
+    tau2guess=tau1guess*.1
+    
     if(debug){
         print("residual: tau1 tau2 A C")
     }
-    decayFit=nls("F ~ (FZero-C)*((abs(A)%%1)*exp(-1*t*tau1)+(1-(abs(A)%%1))*exp(-1*t*tau2)) + C",decayData,start=c(tau1=1,tau2=.1,A=.1,C=0),control=nls.control(warnOnly=TRUE,maxiter=100,minFactor=1/4096),trace=debug)
+    decayFit=nls("F ~ (FZero-C)*((abs(A)%%1)*exp(-1*t*tau1)+(1-(abs(A)%%1))*exp(-1*t*tau2)) + C",decayData,start=c(tau1=tau1guess,tau2=tau2guess,A=aGuess,C=cGuess),control=nls.control(warnOnly=TRUE,maxiter=100,minFactor=1/4096),trace=debug)
     fitData=data.frame(residual=sum(residuals(decayFit)^2),tau1=as.numeric(coef(decayFit)["tau1"]),C=as.numeric(coef(decayFit)["C"]),tau2=as.numeric(coef(decayFit)["tau2"]),A=as.numeric(coef(decayFit)["A"]),converged=decayFit$convInfo$isConv)
 
     measured=data.frame(t=decayData$t,F=decayData$F,curve="measured")
