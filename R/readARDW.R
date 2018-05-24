08f7cdaafa172030be5650fc664ed0704ccfa24d
Nick Stone <stone.nicholas@gmail.com>
2018-05-23 21:08:57 -0400

    Continued to add functionality to read.ardw.

--- R/readARDW.R
+++ R/readARDW.R
@@ -23,13 +23,12 @@ read.ardw=function(filename){
     textSections=getTextSections(textTagIndices,byteStream)
     channels=getChannels(channelHexIndices,byteStream)
     dataSections=getDataSections(nameHexIndices,byteStream,channels)
-    return(list(textSections=textSections,dataSections=dataSections,channels=channels))
+    return(list(headers=textSections,data=dataSections))
 }
 
 getTextSections=function(textTagIndices,byteStream){
     print("Reading Headers")
     textSections=list()
-    textSectionIndex=1
     for(index in textTagIndices){
         thisText=c()
                                         #search for 3 non-null bytes in a row, indicates that data is starting
@@ -39,7 +38,7 @@ getTextSections=function(textTagIndices,byteStream){
         }
                                         #Now slurp up key-value pairs, separated by 0x3a and terminated by 0x0d
         currentIndex=actualStart
-        while(!all(byteStream[currentIndex:(currentIndex+1)]==c(0x00,0x00))){ #Watch for end of text block
+        while(byteStream[currentIndex]!=0x00){ #Watch for end of text block
             thisKey=c()
             while(byteStream[currentIndex]!=0x3a){ #Fill up header
                 thisKey=c(thisKey,byteStream[currentIndex])
@@ -60,8 +59,7 @@ getTextSections=function(textTagIndices,byteStream){
 
             currentIndex=currentIndex+1
         }
-        textSections[[textSectionIndex]]=as.list(thisText)
-        textSectionIndex=textSectionIndex+1
+        textSections=c(textSections,as.list(thisText))
     }
     return(textSections)
 }
@@ -70,19 +68,34 @@ getDataSections=function(nameIndices,byteStream,channels){
     dataHex=c(0x56,0x44,0x41,0x54)
     nameHex=c(0x56,0x4e,0x41,0x4d)
     allPoints=list()
+    pointNames=c()
     allPointsIndex=1
     for(currentIndex in (nameIndices+4)){ #nameIndices contains the indices at the beginning of VNAM, we're interested in what comes after
+
+                                        #First find the first instance of c(0x50,0x00,0x00,0x00,0x58,0x44,0x41,0x54), this will be the end of the last VDAT section
+        maxIndex=currentIndex
+        while(!all(byteStream[maxIndex:(maxIndex+7)]==c(0x50,0x00,0x00,0x00,0x58,0x44,0x41,0x54))){
+            maxIndex=maxIndex+1
+        }
                                         #Search for 3 non null bytes in a row, indicates name is starting (may only need 2 non null bytes in a row, but 3 should work)
         while(any(byteStream[currentIndex:(currentIndex+3)]==0x00)){
             currentIndex=currentIndex+1
         }
         #Now slurp up bytes until the first sequence of 2 null bytes, this will be the name of the dataset
-        nameBytes=readToNull(byteStream,currentIndex)
+        nameBytes=readToSequence(byteStream,currentIndex,c(0x00,0x00))
         currentIndex=currentIndex+length(nameBytes)
         thisName=rawToChar(nameBytes)
         print(thisName)
         dataVectors=list()
         dataVectorIndex=1
+
+                                        #Find termination sequence of the form c(x,0x00,0x00) which is the 4 bytes before the first VDAT tag
+        while(!all(byteStream[currentIndex:(currentIndex+3)]==dataHex)){
+            currentIndex=currentIndex+1
+        }
+
+        terminationSequence=byteStream[(currentIndex-4):(currentIndex-1)]
+        
         #Read up to next VDAT or VNAM tag, then read in the data for VDAT or skip to next name (break out of loop) for VNAM
         while(TRUE){
             #print(paste("currentIndex:",currentIndex,"maxIndex",length(byteStream)))
@@ -102,10 +115,9 @@ getDataSections=function(nameIndices,byteStream,channels){
                 while(any(byteStream[currentIndex:(currentIndex+3)]==0x00)){
                 currentIndex=currentIndex+1
             }
-                                        #Now slurp up bytes until the first sequence of 2 null bytes, this will be the name of the dataset
-                dataBytes=readToNull(byteStream,currentIndex)
+                                        #Now slurp up bytes until the terminationSequence
+                dataBytes=readToSequence(byteStream,currentIndex,terminationSequence,maxIndex)
                 convertedData=readBin(dataBytes,"double",length(dataBytes),4)
-                convertedData=convertedData[1:(length(convertedData)-1)]#Last point seems to be garbage, not sure why
                 dataVectors[[dataVectorIndex]]=convertedData
                 dataVectorIndex=dataVectorIndex+1
                 currentIndex=currentIndex+length(dataBytes)
@@ -119,18 +131,41 @@ getDataSections=function(nameIndices,byteStream,channels){
             }
         }
                                         #Package up the data from this name
-        names(dataVectors)=channels
-        thisPoint=list(name=thisName,data=dataVectors)
-        allPoints[[allPointsIndex]]=thisPoint
+                                        #Make data vectors all the same length
+        targetLength=0
+        for(dVec in dataVectors){
+            targetLength=max(targetLength,length(dVec))
+        }
+        dFrame=data.frame(c())
+        if(length(dataVectors[[1]])<targetLength){
+            dataVectors[[1]]=c(dataVectors[[1]],rep(NA,times=targetLength-length(dataVectors[[1]])))
+        }
+        dFrame=data.frame(dataVectors[[1]])
+        for(i in 2:length(dataVectors)){
+            if(length(dataVectors[[i]])<targetLength){
+                dataVectors[[i]]=c(dataVectors[[i]],rep(NA,times=targetLength-length(dataVectors[[i]])))
+            }
+            dFrame=cbind(dFrame,data.frame(dataVectors[[i]]))
+        }
+        names(dFrame)=channels
+        
+        pointNames=c(thisName,pointNames)
+        allPoints[[allPointsIndex]]=dFrame[1:(dim(dFrame)[1]-1),] #Last point seems to be garbage, not sure why
         allPointsIndex=allPointsIndex+1
     }
+    names(allPoints)=pointNames
     return(allPoints)
 }
-readToNull=function(byteStream,startIndex=0){
-                                        #Read up to the first set of 2 null bytes and return the resulting byte array
+readToSequence=function(byteStream,startIndex=0,stopSequence,maxIndex=NA){
+                                        #Read up to the first instance of stopSequence or up to maxIndex and return the resulting byte array
     theseBytes=c()
     currentIndex=startIndex
-    while(!all(byteStream[currentIndex:(currentIndex+1)]==c(0x00,0x00))){
+    while(!all(byteStream[currentIndex:(currentIndex+(length(stopSequence)-1))]==stopSequence)){
+        if(!is.na(maxIndex)){ #return if currentIndex>maxIndex
+            if(currentIndex>maxIndex){
+                break
+            }
+        }
         theseBytes=c(theseBytes,byteStream[currentIndex])
         currentIndex=currentIndex+1
     }
@@ -143,7 +178,7 @@ getChannels=function(channelHexIndices,byteStream){
         while(byteStream[currentIndex]==0x00){
             currentIndex=currentIndex+1
         }
-        thisChannelBytes=readToNull(byteStream,currentIndex)
+        thisChannelBytes=readToSequence(byteStream,currentIndex,c(0x00,0x00))
         thisChannel=rawToChar(thisChannelBytes)
         channels=c(channels,thisChannel)
     }
