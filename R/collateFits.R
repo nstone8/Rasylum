4d6cb9238afc59bd41cd8d322de86bc43932ab13
Nick Stone <stone.nicholas@gmail.com>
2018-06-26 14:58:00 -0400

    Fixed error where collated fits with one ident would have the wrong name for the ident column

--- R/collateFits.R
+++ R/collateFits.R
@@ -1,11 +1,21 @@
 collateFits=function(fitData){
                                         #Return a single data frame containing all fit values and identifiers. fitData should be an object created by parExtractStiffness or parExtractTimeConst
     allFits=list()
-    allFits[[1]]=cbind(fitData$fits[[1]]$fit$fit,fitData$fits[[1]]$ident)
+    theseIdents=fitData$fits[[1]]$ident
+    if(length(theseIdents)<2){
+        theseIdents=data.frame(theseIdents)
+        names(theseIdents)=names(fitData$fits[[1]]$ident)
+    }
+    allFits[[1]]=cbind(fitData$fits[[1]]$fit$fit,theseIdents)
     numFits=length(fitData$fits)
     if(numFits>1){
-        for(f in 2:numFits){        
-            allFits[[f]]=cbind(fitData$fits[[f]]$fit$fit,fitData$fits[[f]]$ident)
+        for(f in 2:numFits){
+            theseIdents=fitData$fits[[f]]$ident
+            if(length(theseIdents)<2){
+                theseIdents=data.frame(theseIdents)
+                names(theseIdents)=names(fitData$fits[[f]]$ident)
+            }
+            allFits[[f]]=cbind(fitData$fits[[f]]$fit$fit,theseIdents)
         }
     }
     return(buildFrame(allFits,numFits))   
